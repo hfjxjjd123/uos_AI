c76a12891d947e9e31594589811abdb1265baf92
hfjxjjd123 <licbusiness21@gmail.com>
2023-09-24 20:29:55 +0900

    learning -> weight update

--- src/bin/perceptron_learning.rs
+++ src/bin/perceptron_learning.rs
@@ -1,3 +1,5 @@
+use std::{thread,time};
+
 // Experiment with learning AND, OR, and XOR gates (two-dimensional input).
 // Show the learning process using graphs (two-dimensional straight-line graph).
 // Error graph for iterative learning
@@ -9,31 +11,9 @@ const SAMPLE_SIZE: u32 = 4;
 //AND Gate
 fn main(){
     let x = input_x();
-    let learning_rate = 1.0;
-    let iter = 1;
-
+    let y: Vec<i32> = vec![0,0,0,1];
     //random values
-    let mut weights = vec![0.2,0.5];
-    let mut bias: f64 = 0.0;
-
-    let a = forward_propagation(&x, &weights, bias);
-    let o = classification(&a);
-    println!("{:?}", o);
-
-    weights_update(&mut weights, &a, &x, learning_rate);
-    println!("weights of {:?}th iter: {:?}", iter,weights);
-
-    let a = forward_propagation(&x, &weights, bias);
-    let o = classification(&a);
-    println!("{:?}", o);
-
-
-    weights_update(&mut weights, &a, &x, learning_rate);
-    println!("weights of {:?}th iter: {:?}", iter,weights);
-
-    let a = forward_propagation(&x, &weights, bias);
-    let o = classification(&a);
-    println!("{:?}", o);
+    learning(&x, &y);
 
 }
 
@@ -59,15 +39,19 @@ fn forward_propagation(x: &Vec<Vec<i32>>, weights: &Vec<f64>, bias: f64)->Vec<f6
     a_vec
 }
 
-fn weights_update(weights: &mut Vec<f64>, a: &Vec<f64> ,x: &Vec<Vec<i32>>, alpha: f64){
+fn weights_update(weights: &mut Vec<f64>, a: &Vec<f64> ,x: &Vec<Vec<i32>>, alpha: f64)->Vec<f64>{
+    let mut dW_vec = vec![];
+
     for i in 0..x[0].len(){
         let mut dW = 0.0;
         for j in 0..x.len(){
             dW += alpha * a[i] * (1.0 - a[i]) * (x[j][i] as f64);
         } 
         dW = dW/(SAMPLE_SIZE as f64);
+        dW_vec.push(dW);
         weights[i] = weights[i] - dW;
     }
+    dW_vec
 }
 
 fn sigmoid(z: f64)->f64{
@@ -86,4 +70,29 @@ fn classification(a_vec: &Vec<f64>)->Vec<i32>{
         }
     }
     output
+}
+
+fn learning(x: &Vec<Vec<i32>>, y: &Vec<i32>){
+    let mut weights = vec![0.2,0.5];
+    let mut bias: f64 = 0.0;
+    let learning_rate = 0.1;
+
+    let mut iter_count = 0;
+
+    let a = forward_propagation(x, &weights, bias);
+    let mut o = classification(&a);
+    println!("init output: {:?}", o);
+
+    while !o.eq(y){
+        iter_count += 1;
+
+        weights_update(&mut weights, &a, x, learning_rate);
+
+        let a = forward_propagation(x, &weights, bias);
+        let o = classification(&a);
+        println!("weights of {:?}th iter: {:?}", iter_count, weights);
+        println!("{:?}", o);
+        thread::sleep(time::Duration::from_millis(10));
+    }
+
 }
